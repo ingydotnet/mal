00e83607b575c011753a264410cad146be877366
Ingy döt Net <ingy@ingy.net>
2023-02-06 14:47:38 -0500

    WIP - step5_tco
    Use goto and move all special forms into the eval function

--- impls/yamlscript/lib/Eval.pm
+++ impls/yamlscript/lib/Eval.pm
@@ -8,25 +8,58 @@ sub eval {
     my ($ast, $env) = @_;
 
     if (not $ast->isa('list')) {
-        return eval_ast($ast, $env);
+        goto &Eval::eval_ast;
     }
     return $ast if not @$ast;
     my $sym = $ast->[0];
     my $is_sym = ref($sym) eq 'symbol';
-    ($is_sym and $$sym eq 'def!') ?
-        Eval::def($ast, $env) :
-    ($is_sym and $$sym eq 'do') ?
-        Eval::do($ast, $env) :
-    ($is_sym and $$sym eq 'fn*') ?
-        Eval::fn($ast, $env) :
-    ($is_sym and $$sym eq 'if') ?
-        Eval::if($ast, $env) :
-    ($is_sym and $$sym eq 'let*') ?
-        Eval::let($ast, $env) :
-    do {
-        my ($fn, @args) = @{eval_ast($ast, $env)};
-        $fn->(@args);
-    };
+    if ($is_sym and $$sym eq 'def!') {
+        my (undef, $sym, $val) = @$ast;
+        $env->set($$sym, Eval::eval($val, $env));
+    } elsif ($is_sym and $$sym eq 'do') {
+        my (undef, @do) = @$ast;
+        my $last = pop @do;
+        eval_ast(list(\@do), $env);
+        @_ = ($last, $env);
+        goto &Eval::eval;
+    } elsif ($is_sym and $$sym eq 'fn*') {
+        my (undef, $bind, $form) = @$ast;
+        function(
+            sub {
+                $env = Env->new(
+                    outer => $env,
+                    binds => $bind,
+                    exprs => [@_],
+                );
+                @_ = ($form, $env);
+                goto &Eval::eval;
+            }
+        );
+    } elsif ($is_sym and $$sym eq 'if') {
+        my (undef, $cond, $then, $else) = @$ast;
+        if (${boolean(Eval::eval($cond, $env))}) {
+            @_ = ($then, $env);
+        }
+        else {
+            return nil unless defined $else;
+            @_ = ($else, $env);
+        }
+        goto &Eval::eval;
+    } elsif ($is_sym and $$sym eq 'let*') {
+        my ($ast, $env) = @_;
+        $env = Env->new(outer => $env);
+        my (undef, $def, $eval) = @$ast;
+        for (my $i = 0; $i < @$def; $i += 2) {
+            my ($key, $val) = ($def->[$i], $def->[$i+1]);
+            $env->set($$key, Eval::eval($val, $env));
+        }
+        @_ = ($eval, $env);
+        goto &Eval::eval;
+    } else {
+        my $fn;
+        ($fn, @_) = @{eval_ast($ast, $env)};
+        goto &$fn;
+    }
 }
 
 sub eval_ast {
@@ -44,57 +77,6 @@ sub eval_ast {
     else {
         return $ast;
     }
-}
-
-sub def {
-    my ($ast, $env) = @_;
-    my (undef, $sym, $val) = @$ast;
-    $env->set($$sym, Eval::eval($val, $env));
-}
-
-sub do {
-    my ($ast, $env) = @_;
-    my (undef, @do) = @$ast;
-    my $list = eval_ast(list(\@do), $env);
-    pop @$list;
-}
-
-sub fn {
-    my ($ast, $env) = @_;
-    my (undef, $bind, $form) = @$ast;
-    function(
-        sub {
-            $env = Env->new(
-                outer => $env,
-                binds => [@$bind],
-                exprs => [@_],
-            );
-            Eval::eval($form, $env);
-        }
-    );
-}
-
-sub if {
-    my ($ast, $env) = @_;
-    my (undef, $cond, $then, $else) = @$ast;
-    if (${boolean(Eval::eval($cond, $env))}) {
-        Eval::eval($then, $env);
-    }
-    else {
-        return nil unless defined $else;
-        Eval::eval($else, $env);
-    }
-}
-
-sub let {
-    my ($ast, $env) = @_;
-    $env = Env->new(outer => $env);
-    my (undef, $def, $eval) = @$ast;
-    for (my $i = 0; $i < @$def; $i += 2) {
-        my ($key, $val) = ($def->[$i], $def->[$i+1]);
-        $env->set($$key, Eval::eval($val, $env));
-    }
-    Eval::eval($eval, $env);
 }
 
 1;
