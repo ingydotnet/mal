0339ce6e21442d158710e60ed715dd91edf4d729
Ingy döt Net <ingy@ingy.net>
2023-03-10 17:40:38 -0800

    perl.2: Add ENV special form for debugging env

--- impls/perl.2/lib/Eval.pm
+++ impls/perl.2/lib/Eval.pm
@@ -8,86 +8,77 @@ sub eval {
 
     while (1) {
         return eval_ast($ast, $env) unless $ast->isa('list');
-#         unless ($ast->isa('list')) {
-#             $ast = eval_ast($ast, $env);
-#             return $ast;
-#         }
 
         $ast = macroexpand($ast, $env);
 
         return eval_ast($ast, $env) unless $ast->isa('list');
-#         unless ($ast->isa('list')) {
-#             $ast = eval_ast($ast, $env);
-#             return $ast;
-#         }
 
         return $ast unless @$ast;
-#         unless (@$ast) {
-#             return $ast;
-#         }
 
         my ($a0, $a1, $a2, $a3) = @$ast;
         my $sym = (ref($a0) eq 'symbol') ? $$a0 : '';
+
         if ('def!' eq $sym) {
             return $env->set($$a1, Eval::eval($a2, $env));
-#             $ast = Eval::eval($a2, $env);
-#             $ast = $env->set($$a1, $ast);
-#             return $ast;
+
         } elsif ('defmacro!' eq $sym) {
             return $env->set($$a1, macro(Eval::eval($a2, $env)));
-#             $ast = Eval::eval($a2, $env);
-#             $ast = macro($ast);
-#             $ast = $env->set($$a1, $ast);
-#             return $ast;
+
+        } elsif ('ENV' eq $sym) {
+            my $www = {};
+            my $w = $www;
+            my $e = $env;
+            while ($e) {
+                $w->{'+'} = join ' ', sort keys %{$e->{stash}};
+                $w->{'^'} = {};
+                $w = $w->{'^'};
+                $e = $e->{outer};
+            }
+            WWW($www);
+            $ast = nil;
+
         } elsif ('do' eq $sym) {
             my (undef, @do) = @$ast;
             $ast = pop @do;
             eval_ast(list(\@do), $env);
-#             $ast = $ast;
+
         } elsif ('fn*' eq $sym) {
             $a2 = list([symbol('do'), @{$ast}[2..(@$ast-1)] ])
                 if @$ast > 3;
             return function($a1, $a2, $env);
-#             $ast = function($a1, $a2, $env);
-#             return $ast;
+
         } elsif ('if' eq $sym) {
             $ast = ${boolean(Eval::eval($a1, $env))} ? $a2 :
                 defined $a3 ? $a3 : nil;
-#             $ast = Eval::eval($a1, $env);
-#             $ast = boolean($ast);
-#             $ast = $$ast;
-#             if ($ast) {
-#                 $ast = $a2;
-#             } else {
-#                 if (defined $a3) {
-#                     $ast = $a3;
-#                 } else {
-#                     $ast = nil;
-#                 }
-#             }
-#             $ast = $ast;
+
+        } elsif ('let*' eq $sym) {
+            $env = Env->new(outer => $env);
+            for (my $i = 0; $i < @$a1; $i += 2) {
+                $env->set(${$a1->[$i]}, Eval::eval($a1->[$i+1], $env));
+            }
+            $a2 = list([symbol('do'), @{$ast}[2..(@$ast-1)] ])
+                if @$ast > 3;
+            $ast = $a2;
+
         } elsif ('let*' eq $sym) {
             $env = Env->new(outer => $env);
             for (my $i = 0; $i < @$a1; $i += 2) {
                 $env->set(${$a1->[$i]}, Eval::eval($a1->[$i+1], $env));
             }
             $ast = $a2;
-#             $ast = $ast;
+
         } elsif ('macroexpand' eq $sym) {
             return macroexpand($a1, $env);
-#             $ast = macroexpand($a1, $env);
-#             return $ast;
+
         } elsif ('quasiquote' eq $sym) {
             $ast = quasiquote($a1);
-#             $ast = $ast
+
         } elsif ('quasiquoteexpand' eq $sym) {
             return quasiquote($a1);
-#             $ast = quasiquote($a1);
-#             return $ast;
+
         } elsif ('quote' eq $sym) {
             return $a1;
-#             $ast = $a1;
-#             return $ast;
+
         } elsif ('try*' eq $sym) {
             local $@;
             my $val = eval { Eval::eval($a1, $env) };
@@ -110,28 +101,21 @@ sub eval {
                 binds => [$e],
                 exprs => [$err],
             );
+
         } else {
             my ($f, @args) = @{eval_ast($ast, $env)};
             return $f->(@args) if ref($f) eq 'CODE';
-#             if (ref($f) eq 'CODE') {
-#                 $ast = $f->(@args);
-#                 return $ast;
-#             }
-#             XXX [$f, @args] unless ref($f) eq 'function';
             ($ast, $env) = $f->(@args);
-#             $ast = $ast;
         }
     }
 }
 
 sub eval_ast {
     my ($ast, $env) = @_;
-#     $ast =
     $ast->isa('List') ? ref($ast)->new([ map Eval::eval($_, $env), @$ast ]) :
     $ast->isa('Map') ? ref($ast)->new([map Eval::eval($_, $env), %$ast]) :
     $ast->isa('symbol') ? $env->get($$ast) :
     $ast;
-#     $ast;
 }
 
 sub macroexpand {
